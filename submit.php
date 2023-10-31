016c043fc6afdc1fc703e4392495e379f5f990b7
ThiagoCoutinh0 <thiagoalvesdq@gmail.com>
2023-10-31 09:55:20 -0300

    conexao

--- submit.php
+++ submit.php
@@ -1,13 +1,32 @@
 <?php
+$servername = "10.100.53.200";
+$username = "root";
+$password = "LANfpl37792";
+$dbname = "formulario";
+
+// Cria a conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verifica a conexão
+if ($conn->connect_error) {
+  die("Conexão falhou: " . $conn->connect_error);
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['fname'];
     $email = $_POST['email'];
     $password = $_POST['password'];
     $id = $_POST['id'];
 
-    echo "Nome: " . $name . "<br>";
-    echo "Email: " . $email . "<br>";
-    echo "Senha: " . $password . "<br>";
-    echo "ID: " . $id;
+    $sql = "INSERT INTO MyGuests (firstname, email, password, id)
+    VALUES ('$name', '$email', '$password', '$id')";
+
+    if ($conn->query($sql) === TRUE) {
+      echo "Novo registro criado com sucesso";
+    } else {
+      echo "Erro: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
 }
 ?>
